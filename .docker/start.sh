a59429050d82b92356b119544ced9243c7c0bae3
vincentkaleba <vincentkaleba@gmail.com>
2026-04-24 11:53:11 +0200

    feat: add startup script to handle migrations, cache clearing, and directory permissions

--- .docker/start.sh
+++ .docker/start.sh
@@ -7,5 +7,8 @@ php artisan migrate --seed --force
 echo "Clearing caches..."
 php artisan optimize:clear
 
+chown www-data:www-data /var/www/html/database/database.sqlite
+chown www-data:www-data /var/www/html/database
+
 echo "Starting Apache..."
 exec apache2-foreground
